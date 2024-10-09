20db259c940697dbab7fe96ab8c2c9ab9c4be80a
aldanaeguiluz <103043711+aldanaeguiluz@users.noreply.github.com>
2024-10-09 17:15:51 -0500

    producto botica service

--- lib/services/producto_botica.dart
+++ lib/services/producto_botica.dart
@@ -5,12 +5,6 @@ import 'package:mediplan/models/producto_botica.dart';
 class ProductoBoticaService {
   Future<List<ProductoBotica>> fetchAll() async {
     List<ProductoBotica> productos = [];
-<<<<<<< Updated upstream
-    final String response= await rootBundle.loadString("assets/json/data.json");
-    final List<dynamic> data= jsonDecode(response);
-    productos= data.map((map)=> 
-      ProductoBotica.fromMap(map as Map<String, dynamic>)).toList();
-=======
     final String response =
         await rootBundle.loadString('assets/json/data.json');
     print(response);
@@ -19,7 +13,6 @@ class ProductoBoticaService {
     productos = data
         .map((map) => ProductoBotica.fromMap(map as Map<String, dynamic>))
         .toList();
->>>>>>> Stashed changes
     return productos;
   }
 }
