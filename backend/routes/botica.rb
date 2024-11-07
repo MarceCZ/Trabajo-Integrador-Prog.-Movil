c5b2810a00bc200c739ce7ada25db6e612fda4d3
Aldana <103043711+aldanaeguiluz@users.noreply.github.com>
2024-11-07 18:49:03 -0500

    a
    ahora si

--- backend/routes/botica.rb
+++ backend/routes/botica.rb
@@ -18,4 +18,40 @@ get '/botica/listar' do
     resp
     end
 
+# Pantalla 4: boticas con filtro de búsqueda
+get '/botica/listar_filtrado' do
+    status = 200
+    begin
+      busqueda = params['busqueda'] ? "%#{params['busqueda'].downcase}%" : nil
+  
+      query = <<-SQL
+        SELECT 
+          b.*
+        FROM 
+          boticas b
+      SQL
+  
+      if busqueda
+        query += " WHERE LOWER(b.nombre) LIKE ?"
+      end
+  
+      rs = busqueda ? DB[query, busqueda].all : DB[query].all
+  
+      if rs.any?
+        resp = rs.to_json
+      else
+        status = 404
+        resp = 'No hay información de boticas que coincidan con la búsqueda'
+      end
+    rescue StandardError => e
+      status = 500
+      resp = 'Ocurrió un error no esperado al listar las boticas'
+      puts e.message
+    end
+  
+    status status
+    resp
+  end
+  
+
     
